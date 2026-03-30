107c69f02f44dbfd9e1715c72d8c47e4dda75e90
Shehan Salitha <152364600+shehan8461@users.noreply.github.com>
2026-03-30 13:39:32 +0530

    update the content view

--- NurseryConnect/ContentView.swift
+++ NurseryConnect/ContentView.swift
@@ -1,1 +1,20 @@
 
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    var body: some View {
+        HomeDashboardView()
+    }
+}
+
+#Preview {
+    ContentView()
+        .modelContainer(
+            for: [
+                DiaryEntry.self,
+                RatioSession.self,
+            ],
+            inMemory: true
+        )
+}
